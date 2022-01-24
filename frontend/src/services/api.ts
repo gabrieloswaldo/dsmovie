4ca1c42ed2392eb565d2f4cecd6299d53222bd31
gabrieloswaldo <gabriel.oswaldo99@gmail.com>
2022-01-24 19:42:08 -0300

    Create api service with axios

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,9 @@
+import axios, { AxiosInstance } from "axios";
+
+const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+const api: AxiosInstance = axios.create({
+  baseURL: `${BASE_URL}`
+});
+
+export default api;
